ea2f30905f5fc9b4429b8a9e4899ab8a9a5ee300
Joaquin Torre <joa.dev@live.com>
2019-11-28 12:52:30 -0300

    Working setup.py and temporarily ignore VMD
    For now the installation of VMD clouds other issues so I'm
    commenting it out until more important things are working.

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ import os
 import subprocess
 from setuptools import setup
 from distutils.command.build import build as DistutilsBuild
+from setuptools.command.install import install as SetuptoolsInstall
+
 
 
 class BioGraphBuild(DistutilsBuild):
@@ -12,7 +14,8 @@ class BioGraphBuild(DistutilsBuild):
         DistutilsBuild.initialize_options(self)
 
     def finalize_options(self):
-        return None
+        DistutilsBuild.finalize_options(self)
+
 
     def git_clone(self, url, name):
         if os.path.exists(url.split("/")[-1]):
@@ -32,16 +35,17 @@ class BioGraphBuild(DistutilsBuild):
         if "Python 2" in str(result.stdout):
             return "python3"
         return "python"
+
     def run(self):
         # TODO: future -- maybe use directly requirements.txt?
         # https://pip.readthedocs.io/en/1.1/requirements.html#requirements-file-format
         # if the package has a `setup.py develop` then it should work
         self.git_clone("https://github.com/weizhongli/cdhit", "CDHit")
-        self.git_clone("https://github.com/Eigenstate/vmd-python", "VMD")
+        #self.git_clone("https://github.com/Eigenstate/vmd-python", "VMD")
         self.git_clone("https://github.com/getcontacts/getcontacts", "GetContacts")
 
-        subprocess.call(["make", "zlib=no"], cwd=os.path.join(os.getcwd(), "cdhit"))
-        subprocess.call(["python3", "setup.py", "build"], cwd=os.path.join(os.getcwd(), "vmd-python"))
+        #subprocess.call(["make", "zlib=no"], cwd=os.path.join(os.getcwd(), "cdhit"))
+        #subprocess.call(["python3", "setup.py", "build"], cwd=os.path.join(os.getcwd(), "vmd-python"))
         #subprocess.call(["python3", "setup.py", "install"], cwd=os.path.join(os.getcwd(), "vmd-python"))
         # vmd =>netcdf, tk
 
@@ -49,6 +53,17 @@ class BioGraphBuild(DistutilsBuild):
 
         #requirements.txt
         DistutilsBuild.run(self)
+
+class BioGraphInstall(SetuptoolsInstall):
+    def initialize_options(self):
+        SetuptoolsInstall.initialize_options(self)
+
+    def finalize_options(self):
+        SetuptoolsInstall.finalize_options(self)
+
+    def run(self):
+        #self.run_command("build")
+        SetuptoolsInstall.run(self)
 
 setup(
     name="biograph",
@@ -64,6 +79,21 @@ setup(
     },
     cmdclass={
         "build": BioGraphBuild,
-        #"install": BioGraphInstall
+        "install": BioGraphInstall
     },
+    install_requires = [
+        "networkx>=2.3",
+        "pandas>=0.24.2",
+        "numpy>=1.17.2",
+        "scipy>=1.3.0",
+        "requests>=2.18.4",
+        "matplotlib>=3.1.0",
+        "biopython>=0.1.0",
+        "flatten_dict>=0.2.0",
+        "tk",
+        "scikit-learn",
+        "pytest",
+        "seaborn",
+        "cython"
+    ]
 )
